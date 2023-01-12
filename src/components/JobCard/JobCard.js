601a2831717185fa9b84e732ee06d9dd063e13ec
minanodiego <minanodiego5@gmail.com>
2023-01-11 23:58:28 -0500

    add jobslist function

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -1,19 +1,19 @@
 import * as S from "./styles"
 
-function JobCard({params}) {
+function JobCard({category, title, job_type, min_salary, max_salary}) {
   return (
     <S.Wrapper>
       <S.JobCover>
         <p>Imagen</p>
         <S.JobInfo>
           <S.WrapperType>
-            {params.category}
+            {category}
           </S.WrapperType>
-          <h1>{params.title}</h1>
+          <h1>{title}</h1>
           <p>the company name</p>
           <S.WrapperType>
-            <S.ExtraInfo>{params.job_type}</S.ExtraInfo>
-            <S.ExtraInfo>{(params.min_salary/1000).toFixed(1)}k - {(params.max_salary/1000).toFixed(1)}k</S.ExtraInfo>
+            <S.ExtraInfo>{job_type}</S.ExtraInfo>
+            <S.ExtraInfo>{(min_salary/1000).toFixed(1)}k - {(max_salary/1000).toFixed(1)}k</S.ExtraInfo>
           </S.WrapperType>
         </S.JobInfo>
       </S.JobCover>
