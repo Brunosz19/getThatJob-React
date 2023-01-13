60e3f677f86c2a0e339a2bd6a626480c1d77b948
brunosz19 <hesz.bruno05@gmail.com>
2023-01-12 19:45:41 -0500

    ADD: new component called that will render the profile page to update

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -3,6 +3,7 @@ import { colors } from "./styles/colors";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/sidebar";
 import JobPage from "./pages/job-page";
+import RecruiterProfilePage from "./pages/recruiter-profile";
 
 const Container = styled.div`
   display: grid;
@@ -22,7 +23,9 @@ function AuthenticatedApp() {
       <MainContainer>
         <Routes>
           <Route index element={<Navigate to="find-that-job" />} />
+          <Route path="/company/profile" element={<RecruiterProfilePage />} />
           <Route path="/find-that-job" element={<JobPage />} />
+
           {/* <Route path="/categories/:type" element={<CategoriesPage />} />
           <Route path="/transactions" element={<h1>Transactions</h1>} />
           <Route path="/budgets" element={<h1>Budgets</h1>} /> */}
