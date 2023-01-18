95855c62337ddc229d40f1755b413a8905e3941b
minanodiego <minanodiego5@gmail.com>
2023-01-18 16:33:14 -0500

    fix typo in following jobs

--- src/pages/following-page.js
+++ src/pages/following-page.js
@@ -6,8 +6,8 @@ function FollowingPage() {
   return (
     <>
       <h1>Following Page</h1>
-      <p>You are following {user.following_job.length} jobs</p>
-      <JobsList props={user.following_job} />
+      <p>You are following {user.following_jobs.length} jobs</p>
+      <JobsList props={user.following_jobs} />
 
       <p>You are following {user.following_companies.length} companies</p>
       <JobsList props={user.following_companies} type={"company"} />
