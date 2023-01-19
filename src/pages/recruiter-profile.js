6630be837931e8bbdc45dd7cafea8ee167dba696
brunosz19 <hesz.bruno05@gmail.com>
2023-01-19 00:06:01 -0500

    ADD: new button to upload the company logo

--- src/pages/recruiter-profile.js
+++ src/pages/recruiter-profile.js
@@ -1,23 +1,29 @@
 import { Formik } from "formik";
 import { Input, StyledButton, StyledForm } from "../components/input";
 import { useAuth } from "../context/auth-context";
-import { Title } from "../components/utils";
+import { Advice, Note, StyledFileButton, Title } from "../components/utils";
 import { updateRecruiter } from "../services/professional-service";
+import { RiUploadLine } from "react-icons/ri";
 import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
 export default function RecruiterProfilePage() {
-  const [file, setFile] = useState();
   const { user } = useAuth();
+  const [file, setFile] = useState();
+  // const navigate = useNavigate()
+
   function handleChangeFile(values) {
     const formData1 = new FormData();
-    formData1.append("logo", file);
+    if (file) {
+      formData1.append("logo", file)
+    }
     formData1.append("company", values.company);
     formData1.append("email", values.email);
     formData1.append("company_url", values.company_url);
     formData1.append("about", values.about);
 
     updateRecruiter(formData1);
-    // navigate("/");
+    // navigate("/recruiter/profile")
   }
   return (
     <>
@@ -31,6 +37,7 @@ export default function RecruiterProfilePage() {
           file: user.logo,
         }}
         onSubmit={(values) => {
+          console.log(values)
           handleChangeFile(values);
         }}
       >
@@ -39,20 +46,74 @@ export default function RecruiterProfilePage() {
             style={{ gap: "8px", padding: "0 120px" }}
             onSubmit={handleSubmit}
           >
-            <div style={{ display: "flex", gap: "10px" }}>
-              <img src={user.logo_url} />
+            <div style={{ display: "flex", gap: "11px" }}>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "10px",
+                  width: "76px",
+                  height: "76px",
+                  boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.25)",
+                  borderRadius: "8px",
+                }}
+              >
+                <img
+                  src={user.logo_url}
+                  width={"75px"}
+                  height={"75px"}
+                  alt="Logo of the company"
+                />
+              </div>
+
+              <div>
+                <Note style={{ margin: "0", height: "18px" }}>
+                  Upload/Update your CV
+                </Note>
+                <label
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "16px",
+                    alignItems: "flex-end",
+                  }}
+                  for="file"
+                >
+                  <StyledFileButton
+                    style={{
+                      background: "#F48FB1",
+                      color: "white",
+                      width: "164px",
+                      height: "36px",
+                      borderRadius: "8px",
+                      padding: "10px",
+                    }}
+                    type="button"
+                  >
+                    <RiUploadLine />
+                    Choose a file
+                  </StyledFileButton>
+                  {file ? (
+                    <Note style={{ margin: "0" }}>{file.name}</Note>
+                  ) : (
+                    <Note style={{ margin: "0" }}>No file choosen</Note>
+                  )}
+                </label>
+                <Advice style={{ marginTop: "8px" }}>
+                  Only PDF. Max size 5MB
+                </Advice>
+
+                <input
+                  style={{ display: "none" }}
+                  id="file"
+                  name="file"
+                  type="file"
+                  for="file"
+                  value={values.file}
+                  onChange={(event) => setFile(event.target.files[0])}
+                ></input>
+              </div>
             </div>
 
-            <label for="file"> file </label>
-            <input
-              id="file"
-              name="file"
-              type="file"
-              value={values.file}
-              onChange={(event) => setFile(event.target.files[0])}
-            ></input>
-
-            
             <Input
               name="email"
               type="filename"
