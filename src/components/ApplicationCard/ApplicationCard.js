3d2b99fe0a7bd7f8c09d734d8a6fc5cd045292fe
minanodiego <minanodiego5@gmail.com>
2023-01-19 12:34:56 -0500

    refactor component

--- src/components/ApplicationCard/ApplicationCard.js
+++ src/components/ApplicationCard/ApplicationCard.js
@@ -1,38 +1,42 @@
-import { useState } from "react"
-import * as S from "./styles"
-import FilterCheckboxs from "./filters";
+import { useState } from "react";
+import * as S from "./styles";
+// import FilterCheckboxs from "./filters";
 import JobCompany from "./JobCompany";
 import JobInfo from "./JobInfo";
 import JobStatus from "./JobStatus";
-import ModalList from "../ModalList"
+import ModalList from "../ModalList";
 
-function ApplicationCard({appliedJobs}) {
+function ApplicationCard({
+  title,
+  category,
+  job_type,
+  min_salary,
+  max_salary,
+}) {
   const [detailsOpen, setDetailsOpen] = useState(false);
-
+  console.log(title);
   function ExpandedButton() {
     setDetailsOpen(!detailsOpen);
   }
 
   return (
     <div>
-      <S.ApplicationsTitle>Your Applications</S.ApplicationsTitle>
-      <div>
-        <FilterCheckboxs />
-        <S.ApplicationsFound>
-          {appliedJobs?.length} 1 applications found
-        </S.ApplicationsFound>
-        <S.ShadowBox>
-          <S.InfoContainer>
-            <JobCompany title={appliedJobs?.job.title}/>
-            <JobInfo category={appliedJobs?.job.category} job_type={appliedJobs?.job.job_type} min_salary={appliedJobs?.job.min_salary} max_salary={appliedJobs?.job.max_salary}/>
-            <JobStatus onClick={ExpandedButton} />
-          </S.InfoContainer>
-          <ModalList detailsOpen={detailsOpen} />
-        </S.ShadowBox>
-      </div>
+      <S.ShadowBox>
+        <S.InfoContainer>
+          <JobCompany title={title} />
+          <JobInfo
+            category={category}
+            job_type={job_type}
+            min_salary={min_salary}
+            max_salary={max_salary}
+          />
+          <JobStatus onClick={ExpandedButton} />
+        </S.InfoContainer>
+        <ModalList detailsOpen={detailsOpen} />
+      </S.ShadowBox>
+      {/* </div> */}
     </div>
   );
-
 }
 
-export default ApplicationCard
+export default ApplicationCard;
