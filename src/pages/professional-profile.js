77ae674ce302886ab52d289d761d420ecddc9aa1
brunosz19 <hesz.bruno05@gmail.com>
2023-01-19 00:05:47 -0500

    ADD: new button to upload the cv

--- src/pages/professional-profile.js
+++ src/pages/professional-profile.js
@@ -1,47 +1,62 @@
 import { Formik } from "formik";
 import { Input, StyledButton, StyledForm } from "../components/input";
 import { useAuth } from "../context/auth-context";
-import { Title } from "../components/utils";
-import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import { Advice, Note, StyledFileButton, SubTitle, Title } from "../components/utils";
+import { RiUploadLine } from "react-icons/ri";
+import { updateUser } from "../services/professional-service";
+import { useState } from "react";
+// import { RiMoneyDollarCircleFill } from "react-icons/ri";
 
 export default function ProfessionalProfilePage() {
   const { user } = useAuth();
+  const [file, setFile] = useState();
+
+  function handleChangeFile(values) {
+    const formData1 = new FormData();
+    if (file) {
+      formData1.append("cv", file);
+    }
+    formData1.append("email", values.email);
+    formData1.append("password", values.password);
+    formData1.append("password_confirmation", values.password_confirmation);
+    formData1.append("name", values.name);
+    if (values.phone) {
+      formData1.append("phone", values.phone);
+    }
+    formData1.append("birthdate", values.birthdate);
+    formData1.append("link", values.link);
+    formData1.append("title", values.title);
+    formData1.append("experience", values.experience);
+    formData1.append("education", values.education);
+
+    updateUser(formData1);
+  }
+
   return (
     <>
       <Title>Profile</Title>
       <Formik
         initialValues={{
           email: user.email,
-          company: user.company,
-          company_url: user.company_url,
-          about: user.about,
-        }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
-
-          if (!values.password) {
-            errors.password = "Required";
-          } else if (values.password.length <= 6) {
-            errors.password = "Invalid password";
-          }
-          return errors;
+          name: user.name,
+          phone: user.phone,
+          birthday: user.birthday,
+          link: user.link,
+          title: user.title,
+          experience: user.experience,
+          education: user.education,
         }}
         onSubmit={(values) => {
+          handleChangeFile(values);
           console.log(values);
         }}
       >
         {({ values, errors, touched, handleChange, handleSubmit }) => (
           <StyledForm
-            style={{ gap: "16px", padding: "0 120px" }}
+            style={{ gap: "4px", padding: "0 120px" }}
             onSubmit={handleSubmit}
           >
+            <SubTitle>Personal information</SubTitle>
             <Input
               name="email"
               type="email"
@@ -52,24 +67,6 @@ export default function ProfessionalProfilePage() {
             />
             {errors.email && touched.email && errors.email}
             <Input
-              name="password"
-              type="password"
-              value={values.password}
-              onChange={handleChange}
-              placeholder="******"
-              label="Password"
-            />
-            {errors.password && touched.password && errors.password}
-            <Input
-              name="password_confirmation"
-              type="password"
-              value={values.password_confirmation}
-              onChange={handleChange}
-              placeholder="******"
-              label="Password Confirmation"
-            />
-            {errors.password && touched.password && errors.password}
-            <Input
               name="name"
               type="text"
               value={values.name}
@@ -78,15 +75,18 @@ export default function ProfessionalProfilePage() {
               label="name"
             />
             {errors.name && touched.name && errors.name}
-            <Input
-              name="phone"
-              type="number"
-              value={values.phone}
-              onChange={handleChange}
-              placeholder="+XXXXXXXXX"
-              label="phone"
-            />
-            {errors.phone && touched.phone && errors.phone}
+            <div>
+              <Input
+                name="phone"
+                type="tel"
+                value={values.phone}
+                onChange={handleChange}
+                placeholder="+XXXXXXXXX"
+                label="phone"
+              />
+              {errors.phone && touched.phone && errors.phone}
+              <Advice>+[country code][number]</Advice>
+            </div>
             <Input
               name="birthday"
               type="date"
@@ -105,6 +105,9 @@ export default function ProfessionalProfilePage() {
               label="Linkedin URL"
             />
             {errors.link_url && touched.link_url && errors.link_url}
+
+            <SubTitle style={{marginTop: "32px"}}>Professional information</SubTitle>
+            <Note style={{width: "100%", margin: "0"}}>Changes made here will be reflected in your future applications</Note>
             <Input
               name="title"
               type="text"
@@ -132,6 +135,66 @@ export default function ProfessionalProfilePage() {
               label="Education"
             />
             {errors.education && touched.education && errors.education}
+            <div>
+                <Note style={{ margin: "0", height: "18px" }}>
+                  Upload/Update your CV
+                </Note>
+                <label
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "16px",
+                    alignItems: "flex-end",
+                  }}
+                  for="file"
+                >
+                  <StyledFileButton
+                    style={{
+                      background: "#F48FB1",
+                      color: "white",
+                      width: "164px",
+                      height: "36px",
+                      borderRadius: "8px",
+                      padding: "10px",
+                    }}
+                    type="button"
+                  >
+                    <RiUploadLine />
+                    Choose a file
+                  </StyledFileButton>
+                  {file ? (
+                    <Note style={{ margin: "0" }}>{file.name}</Note>
+                  ) : (
+                    <Note style={{ margin: "0" }}>No file choosen</Note>
+                  )}
+                </label>
+                <Advice style={{ marginTop: "8px" }}>
+                  Only PDF. Max size 5MB
+                </Advice>
+
+                <input
+                  style={{ display: "none" }}
+                  id="file"
+                  name="file"
+                  type="file"
+                  for="file"
+                  value={values.file}
+                  onChange={(event) => setFile(event.target.files[0])}
+                ></input>
+              </div>
+            <StyledButton
+              style={{
+                width: "165px",
+                height: "40px",
+                color: "white",
+                background: "#F48FB1",
+                padding: "8px 16px",
+                gap: "8px",
+              }}
+              type="submit"
+            >
+              update profile
+            </StyledButton>
           </StyledForm>
         )}
       </Formik>
