f5e5727ca5a9125c33784a4302700f3ccf360eb1
Carlos Mendoza <zerotyr3@gmail.com>
2023-01-18 20:08:57 -0500

    Add: following job button function to chage color

--- src/pages/job-details-page.js
+++ src/pages/job-details-page.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { getJob } from "../services/job-services";
 import { Link } from "react-router-dom";
+import { getFollowings } from "../services/following-services";
 
 
 const BackButton = styled("button")`
@@ -82,6 +83,7 @@ export default function JobDetails(){
 
     const navigate = useNavigate();
     const [job, setJob] = useState();
+    const [followings, setFollowings] = useState();
     const { id } = useParams();
 
     function ApplicationJon(job){
@@ -89,8 +91,11 @@ export default function JobDetails(){
     }
 
     useEffect(() => {
+        getFollowings().then(setFollowings)
         getJob(id).then(setJob).catch(console.log)
     }, []);
+
+    const isFollow = followings?.some(follows => follows.job_id === job?.id)
 
     return (
         <div>
@@ -102,7 +107,7 @@ export default function JobDetails(){
                         <div>
                             <JobSubTitle style={{color: "#373737", marginBottom: "0",}}>{job?.company_info.name}</JobSubTitle>
                             <div style={{display: "flex", alignItems: "center"}}>
-                                <div style={{borderRadius: "50%", width: "20px", height: "20px", background: "#F48FB1", display: "flex", alignItems: "center", justifyContent: "center", color: "white",}}><RiFocus3Line/></div>
+                                <div style={{borderRadius: "50%", width: "20px", height: "20px", background: (isFollow) ? "#F48FB1" : "white", display: "flex", alignItems: "center", justifyContent: "center", color: (isFollow) ? "white" : "#616161",}}><RiFocus3Line/></div>
                                 <BackButton>FOLLOWING</BackButton>
                             </div>
                         </div>
