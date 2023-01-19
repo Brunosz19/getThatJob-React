15d2d46f5ece6a32eb8f427bd06537663d15a95a
brunosz19 <hesz.bruno05@gmail.com>
2023-01-19 14:35:48 -0500

    ADD: new styled component to add a subtitle

--- src/components/JobsList/JobsList.js
+++ src/components/JobsList/JobsList.js
@@ -1,18 +1,22 @@
 import JobCard from "../JobCard";
 import CompanyCard from "../CompanyCard";
 import { Wrapper } from "./styles";
+import { LoginText } from "../utils";
 
 function JobsList({ props, type }) {
   return (
-    <Wrapper>
-      {props?.map(({ ...props }) =>
-        type === "company" ? (
-          <CompanyCard key={props.id} {...props} />
-        ) : (
-          <JobCard key={props.id} {...props} />
-        )
-      )}
-    </Wrapper>
+    <>
+      <LoginText style={{marginTop: "24px", marginBottom: "8px"}}> {props?.length} jobs for you </LoginText>
+      <Wrapper>
+        {props?.map(({ ...props }) =>
+          type === "company" ? (
+            <CompanyCard key={props.id} {...props} />
+          ) : (
+            <JobCard key={props.id} {...props}  />
+          )
+        )}
+      </Wrapper>
+    </>
   );
 }
 
