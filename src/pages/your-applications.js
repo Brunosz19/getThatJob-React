a9f1e89b68c8d600aad1b4a538569a8de7a51ec7
minanodiego <minanodiego5@gmail.com>
2023-01-18 23:58:11 -0500

    add useEffect to fetch applied jobs in applications page

--- src/pages/your-applications.js
+++ src/pages/your-applications.js
@@ -1,5 +1,11 @@
+import { useEffect, useState } from "react";
 import ApplicationCard from "../components/ApplicationCard";
+import { GetAppliedJobs } from "../services/job-services";
 
 export default function YourApplications() {
-  return <ApplicationCard />
+  const [appliedJobs, setAppliedJobs] = useState(null)
+  useEffect(() => {
+    GetAppliedJobs().then(setAppliedJobs).catch(console.log)
+  }, []);
+  return <ApplicationCard appliedJobs={appliedJobs?.at(0)}/>
 }
